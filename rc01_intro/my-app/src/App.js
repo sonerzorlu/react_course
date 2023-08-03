51bb16433d808e279f07a9a79439f3bdf6a66dba
Soner Zorlu <sonerzorlu@gmail.com>
2023-08-03 21:34:40 +0300

    template_ first attempt

---
+++ rc01_intro/my-app/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Hi React</h1>
+      <h3>React is a js library</h3>
+      <h2>this is a react repeat course</h2>
+    </div>
+  )
+}
+
+export default App
